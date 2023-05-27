4e4057b13b1cca1858eccda63bceb516a293299e
DAKDesigns <129184268+DAKDesigns@users.noreply.github.com>
2023-05-27 08:24:56 -0600

    Add support for lowercase entry by user

--- logic.js
+++ logic.js
@@ -11,38 +11,47 @@ function playRound (playerSelection, computerSelection) {
     switch (playerSelection + computerSelection) {
 
     case "RockScissors":
+    case "rockScissors":
         return "You Win! Rock beats Scissors";
         break;
 
     case "RockPaper":
+    case "rockPaper":
         return "You Lose! Paper beats Rock";
         break;
 
     case "RockRock":
+    case "rockRock":
         return "Draw! Rock cannot beat Rock";
         break;
 
     case "PaperRock":
+    case "paperRock":
         return "You Win! Paper beats Rock";
         break;
         
     case "PaperScissors":
+    case "paperScissors":
         return "You Lose! Scissors beats Paper";
         break;
 
     case "PaperPaper":
+    case "paperPaper":
         return "Draw! Paper cannot beat Paper";
         break;
 
     case "ScissorsRock":
+    case "scissorsRock":
         return "You Lose! Rock beats Scissors";
         break;
             
     case "ScissorsScissors":
+    case "scissorsScissors":
         return "Draw! Scissors cannot beat Scissors";
         break;
     
     case "ScissorsPaper":
+    case "scissorsPaper":
         return "You Win! Scissors beats Paper";
         break;
 
